10dd18557b78d19bdc34f554b731163c3e8e8c9c
ParamveerChattha <paramveersingh08.92@gmail.com>
2017-09-26 13:30:23 +0530

    update MapChange.java in Assignment

--- Assignment/MapChange.java
+++ Assignment/MapChange.java
@@ -2,6 +2,7 @@ import java.util.*;
 class Input{
   String Key,Value;
   Map<String,String> Details = new HashMap<>();
+  Map<String,String> finalOutput= new HashMap<>();
   Scanner sc = new Scanner(System.in);
   public void read(){
   System.out.println("enter empty string to end");
@@ -22,21 +23,36 @@ class Input{
     read();
     String locKey,locValue;
     int count =0;
+    List<String> keyList = new ArrayList<>();
+    List<String> valList = new ArrayList<>();
     Set<String> detailsKey = Details.keySet();
     int size= detailsKey.size();
     System.out.println(size);
     for(String Key : detailsKey ){
-      count ++;
-          locKey= Details.get(Key);
-          locValue= Key;
-          if(size%2==1 && count == ((size/2)+1))
-            {
-            locKey=Key;
-            locValue=Details.get(Key);
-            }
-      System.out.println(locKey + " : "+ locValue);
+      keyList.add(Key);
+      valList.add(Details.get(Key));
+      //System.out.println(locKey + " : "+ locValue);
 //      System.out.println(Key + " : " +Details.get(Key));
-  }
+      }
+      // for(String x: keyList)
+      // {System.out.println(x);}
+      // for(String y: valList)
+      // {System.out.println(y);}
+      if(size%2==0){
+        String tempVal1,tempVal2;
+        for(int i=0;i<size;i++){
+          tempVal1 = valList.get(i+1);
+          tempVal2 = valList.get(i);
+          finalOutput.put(keyList.get(i),tempVal1);
+          i++;
+          finalOutput.put(keyList.get(i),tempVal2);
+        }
+        Set<String> fKey = finalOutput.keySet();
+        for(String Key : fKey)
+        {
+          System.out.println(Key + " " + finalOutput.get(Key));
+        }
+      }
 }
 }
 class MapChange {
